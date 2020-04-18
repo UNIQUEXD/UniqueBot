f4483776d45653544c23dbf7b026c6ac26f8db5e
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-04-18 21:13:35 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -152,7 +152,7 @@ async def carbon_api(e):
 
          file,
 
-         caption="<< Here's your carbon, \n Carbonised by [IndianBot](https://www.github.com/indianbhaiya/IndianBot)>> ",
+         caption="<< Here's your carbon, \n Carbonised by [UniqueBoot](https://github.com/UNIQUEXD/UniqueBot)>> ",
 
          force_document=True,
 
